b2d625eaa4d5b758be01e8f51b80515fd418d485
Kangyun Wang <kevinwang8011@hotmail.com>
2019-07-06 22:44:24 +0200

    Add some breakpoints to thme

--- expandingCircleMenu/src/styles/theme.js
+++ expandingCircleMenu/src/styles/theme.js
@@ -1,12 +1,23 @@
 const theme = {
-  transitions: {
-    duration: {
-      // currently arbitrarily added
-      short: 100,
-      normal: 300,
-      long: 500,
-    },
-  },
+	colors: {},
+	breakpoints: {
+		small: style => `@media (max-aspect-ratio: 1/1) { ${style} }`,
+		medium: style =>
+			`@media (max-aspect-ratio: 1/1) and (min-height: 30rem) and (max-width: 40rem) { ${style} }`,
+		large: style =>
+			`@media (min-aspect-ratio: 1/1) and (min-height: 40rem) { ${style} }`,
+		wide: style =>
+			`@media (min-aspect-ratio: 1/1) and (min-height: 30rem) and (min-width: 60rem) { ${style} }`,
+	},
+	sizes: {},
+	transitions: {
+		duration: {
+			// currently arbitrarily added
+			short: 100,
+			normal: 300,
+			long: 500,
+		},
+	},
 };
 
 export default theme;
