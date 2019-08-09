3442699b632755c836fed72699ffcde98d1711b2
Kangyun Wang <kevinwang8011@hotmail.com>
2019-08-09 19:14:00 +0200

    Implement fileAnalyse from pitch-anlayser (not live yet)

--- pitchAnalyserFile/src/pages/index.js
+++ pitchAnalyserFile/src/pages/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import Container from '../components/general/Container';
+// import DropContainer from '../components/DropContainer';
 
-export default () => (
-  <Container>
-    <h1>Title</h1>
-    <p>Paragraph</p>
-  </Container>
-);
+import { fileAnalyse } from 'pitch-analyser';
+
+class MainPage extends React.PureComponent {
+  static propTypes = {};
+  static defaultProps = {};
+
+  fileInputRef = React.createRef();
+
+  state = {};
+
+  handleClick = () => {
+    if (!this.fileInputRef.current.files[0]) return;
+
+    // const audioURL = URL.createObjectURL(this.fileInputRef.current.files[0]);
+
+    fileAnalyse({
+      audioFile: this.fileInputRef.current.files[0],
+      callback: result => {
+        console.log(result);
+      },
+      returnCents: true,
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        {/* <DropContainer inputRef={this.fileInputRef} /> */}
+        <input
+          accept="audio/*"
+          name="audio_file"
+          ref={this.fileInputRef}
+          type="file"
+        ></input>
+        <button onClick={this.handleClick}>Analyze</button>
+      </Container>
+    );
+  }
+}
+
+export default MainPage;
